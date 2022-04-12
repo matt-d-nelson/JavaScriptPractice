d00ddbc90c2fa9514d886eb9f79475beb6668479
Matt Nelson <mattnelson6992@gmail.com>
2022-04-12 13:02:46 -0500

    codeWars3

--- codeWars/creditCardMask.js
+++ codeWars/creditCardMask.js
@@ -86,3 +86,23 @@ function persistence(num) {
 }
 
 console.log(persistence(39));
+
+/*
+Write a function that will check if two given characters are the same case.
+
+If either of the characters is not a letter, return -1
+If both characters are the same case, return 1
+If both characters are letters, but not the same case, return 0
+*/
+
+function sameCase(a, b){
+  let singleLet = /^[A-Za-z]+$/;
+  if(singleLet.test(a) && singleLet.test(b)) {
+    if(a.toLowerCase() == a && b.toLowerCase() == b) return 1;
+    if(a.toUpperCase() == a && b.toUpperCase() == b) return 1; 
+  } else return -1;
+  return 0;
+}
+
+
+console.log(sameCase(' ','6'));
